feb11ccc3d29864547189a87e8ed84f25a2b66dc
Unknown <jonas.roettger@gmx.net>
2018-02-06 12:29:13 +0100

    change mirror coordinates y & x, z is good

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Controller : MonoBehaviour {
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+    private SteamVR_TrackedObject trackedObj;
+    public Vector3 Position;
+    public Vector3 mirroredPosition;
+    public GameObject mirroredCube;
+
+    // Use this for initialization
+    void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		Position = trackedObj.transform.position;
+        mirroredPosition = -Position;
+        mirroredCube.transform.position = mirroredPosition;
+	}
+}
